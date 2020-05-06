2a67a3245eb861188c792b62577ab0d7ee30daec
Aryan <sep2001aryan@yahoo.com>
2020-05-06 07:25:43 +0530

    created a todo list using react-redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import PrivateRoute from "./UI/Components/PrivateRoute";
+import Todos from "./UI/Routes/Todo";
+import SignIn from "./UI/Routes/Signin";
+import { Switch, Route } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return (
+        <div
+            style={{
+                textAlign: "center",
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <h1>Redux Todo App</h1>
+            <Switch>
+                <PrivateRoute path="/todos">
+                    <Todos />
+                </PrivateRoute>
+                <Route path="/">
+                    <SignIn />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,112 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import firebase from "firebase/app";
+// import "firebase/auth";
+// import "firebase/firestore";
+// import { createStore } from "redux";
+// import { Provider } from "react-redux";
+// import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+// import { createFirestoreInstance } from "redux-firestore";
+// import { rootReducer } from "./ducks/reducer";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+
+// // setting up firebasse configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDSaaz2S-HfkCFq6BD90VpWt3OctShADyQ",
+//     authDomain: "assignment-208fe.firebaseapp.com",
+//     databaseURL: "https://assignment-208fe.firebaseio.com",
+//     projectId: "assignment-208fe",
+//     storageBucket: "assignment-208fe.appspot.com",
+//     messagingSenderId: "614718936508",
+//     appId: "1:614718936508:web:c9a6c686cf3f5937aec7b9",
+//     measurementId: "G-0KB46PE1KQ",
+// };
+
+// // setting up user config
+
+// const rrconfig = {
+//     userProfile: "user",
+//     useFirestoreForProfile: true,
+// };
+
+// firebase.initializeApp(firebaseConfig);
+// firebase.firestore();
+
+// const initialState = {};
+// const store = createStore(rootReducer, initialState);
+
+// const rrfPrpos = {
+//     firebase,
+//     config: rrconfig,
+//     dispatch: store.dispatch,
+//     createFirestoreInstance,
+// };
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <Provider store={store}>
+//             <reactReduxFirebaseProvider {...rrfPrpos}>
+//                 <BrowserRouter>
+//                     <App />
+//                 </BrowserRouter>
+//             </reactReduxFirebaseProvider>
+//         </Provider>
+//     </React.StrictMode>,
+//     document.getElementById("root")
+// );
+
+import React from "react";
+import ReactDOM from "react-dom";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import { createFirestoreInstance } from "redux-firestore";
+import { rootReducer } from "./ducks/reducer";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDSaaz2S-HfkCFq6BD90VpWt3OctShADyQ",
+    authDomain: "assignment-208fe.firebaseapp.com",
+    databaseURL: "https://assignment-208fe.firebaseio.com",
+    projectId: "assignment-208fe",
+    storageBucket: "assignment-208fe.appspot.com",
+    messagingSenderId: "614718936508",
+    appId: "1:614718936508:web:c9a6c686cf3f5937aec7b9",
+    measurementId: "G-0KB46PE1KQ",
+};
+
+const rrfConfig = {
+    userProfile: "users",
+    useFirestoreForProfile: true,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+const initialState = {};
+const store = createStore(rootReducer, initialState);
+
+const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance, //since we are using Firestore
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <ReactReduxFirebaseProvider {...rrfProps}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </ReactReduxFirebaseProvider>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
